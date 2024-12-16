3d863d7a564734e0802963c0b2dc1e69565c55e6
Paul Haedrich <38889179+berrysauce@users.noreply.github.com>
2024-12-16 22:20:46 +0100

    Updated domain validation for longer domains

--- api/src/utils.ts
+++ api/src/utils.ts
@@ -5,7 +5,7 @@
  * @returns {boolean} True if the domain is valid, false otherwise.
  */
 function isValidDomain(domain: string): boolean {
-    const domainRegex = /^(?!:\/\/)([a-zA-Z0-9-_]{1,63}\.)+[a-zA-Z]{2,6}$/;
+    const domainRegex = /^(?!:\/\/)([a-zA-Z0-9-_]{1,63}\.)+[a-zA-Z0-9-]{2,}$/;
     return domainRegex.test(domain);
 }
 
